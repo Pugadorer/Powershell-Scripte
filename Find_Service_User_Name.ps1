103e39ddae4e8919d2690b48d86364d15bb8d290
m.shamrytskyy <mychailo96@gmx.de>
2023-02-03 09:33:16 +0100

    Erste skripte
    Erste Skripte die ich für die arbeit geschrieben hatte.

---
+++ Find_Service_User_Name.ps1
@@ -0,0 +1,4 @@
+﻿$servers = @(   #Servers to check#
+ ) foreach ($server in $servers){ 
+Get-WmiObject win32_service | sort-object -property startname |where-object{$_.startname -ne "localservice" -and $_.startname -ne "Localsystem" -and $_.startname -ne "networkservice"}|  Select-Object -Property Name, startname |export-csv -Path C:\temp\$server.csv -Delimiter ';'
+}
---
+++ Find_replace.ps1
@@ -0,0 +1,16 @@
+﻿$Dateipfadtest = Test-Path $Dateipfad
+$Dateipfad = "C:\temp\" + $prog.ToString()
+$prog = "test.txt"
+$neu = "I:\Working\Mischa\Test2.txt"
+$progneu = Split-Path "\\server\share\test.txt" -leaf 
+
+if($Dateipfadtest -eq "True")
+{
+    Remove-Item -Path $Dateipfad
+    Copy-item "\\Server\Share\test.txt" -Destination "C:\temp"
+    write-host "Datei" $prog "ersetzt durch" $progneu 
+}
+else {
+
+Write-host $prog + "ist nicht auf" $env:computername.ToString( )
+                }
